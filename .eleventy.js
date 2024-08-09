4fb00595a09054b03047fe31b1d7e3086dc707b6
Leon <lowpolygonum@gmail.com>
2024-08-09 22:54:08 +0200

    first pass front end + content integration

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,8 @@ const eleventySass = require("@11tyrocks/eleventy-plugin-sass-lightningcss");
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPlugin(eleventySass);
+  eleventyConfig.addPassthroughCopy("src/media");
+  eleventyConfig.addPassthroughCopy("src/content");
 
   return {
     dir: {
