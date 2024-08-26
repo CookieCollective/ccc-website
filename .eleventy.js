cf05ae417cb1d1fdbe3c063ac5d137a4175f10c4
Raphaël Forment <raphael.forment@gmail.com>
2024-08-26 11:41:47 +0200

    Add support for syntax highlighting (PrismJS)

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,8 @@
 const eleventySass = require("@11tyrocks/eleventy-plugin-sass-lightningcss");
+const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 
 module.exports = function (eleventyConfig) {
+	eleventyConfig.addPlugin(syntaxHighlight);
   eleventyConfig.addPlugin(eleventySass);
   eleventyConfig.addPassthroughCopy("src/media");
   eleventyConfig.addPassthroughCopy("src/content");
