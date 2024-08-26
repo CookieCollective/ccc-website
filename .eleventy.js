37f4f1c322f04d66a0520ccc2708d1b0db584245
Raphaël Forment <raphael.forment@gmail.com>
2024-08-27 00:31:40 +0200

    Quelques progrès

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,13 @@
 const eleventySass = require("@11tyrocks/eleventy-plugin-sass-lightningcss");
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
+const mdxPlugin = require("@jamshop/eleventy-plugin-mdx");
 
 module.exports = function (eleventyConfig) {
 	eleventyConfig.addPlugin(syntaxHighlight);
   eleventyConfig.addPlugin(eleventySass);
   eleventyConfig.addPassthroughCopy("src/media");
   eleventyConfig.addPassthroughCopy("src/content");
+  eleventyConfig.addPlugin(mdxPlugin);
   return {
     dir: {
       input: "src",
---
+++ src/_data/processedProjects.js
@@ -0,0 +1,41 @@
+const fs = require('fs');
+const path = require('path');
+const matter = require('gray-matter');
+const slugify = require('slugify');
+
+module.exports = function() {
+  const projectsJson = JSON.parse(fs.readFileSync('./src/_data/projects.json', 'utf8'));
+
+  function titleToSlug(title) {
+    return slugify(title, { lower: true, strict: true });
+  }
+
+  function loadMarkdownContent(projectSlug, fileName) {
+    const fullPath = path.join('./src/content/projects', projectSlug, fileName);
+    if (fs.existsSync(fullPath)) {
+      const fileContent = fs.readFileSync(fullPath, 'utf8');
+      const { content } = matter(fileContent);
+      return content;
+    }
+    return `Content not found for ${fileName}`;
+  }
+
+  const processedProjects = projectsJson.map(project => {
+    const projectSlug = titleToSlug(project.folder);
+
+    if (project.videast) {
+      project.videast.bio = loadMarkdownContent(projectSlug, 'videast_bio.html');
+      project.videast.description = loadMarkdownContent(projectSlug, 'videast_description.html');
+    }
+
+    if (project.musician) {
+      project.musician.bio = loadMarkdownContent(projectSlug, 'musician_bio.html');
+      project.musician.description = loadMarkdownContent(projectSlug, 'musician_description.html');
+    }
+
+    return project;
+  });
+
+  console.log('processedProjects', processedProjects);
+  return processedProjects;
+};
