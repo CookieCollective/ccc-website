9efdc2d8150c80ba941927fa14be1f4e8c89bc05
Raphaël Forment <raphael.forment@gmail.com>
2024-08-26 10:58:58 +0200

    Adding basic random page display

--- .eleventy.js
+++ .eleventy.js
@@ -4,7 +4,6 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPlugin(eleventySass);
   eleventyConfig.addPassthroughCopy("src/media");
   eleventyConfig.addPassthroughCopy("src/content");
-
   return {
     dir: {
       input: "src",
