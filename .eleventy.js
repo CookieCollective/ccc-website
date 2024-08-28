92b4c8e7e6c4ecc41eb60c3e3f10008566fd1312
Raphaël Forment <raphael.forment@gmail.com>
2024-08-28 11:09:29 +0200

    Version de travail un peu sale

--- .eleventy.js
+++ .eleventy.js
@@ -8,6 +8,15 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/media");
   eleventyConfig.addPassthroughCopy("src/content");
   eleventyConfig.addPlugin(mdxPlugin);
+  eleventyConfig.addFilter("shuffle", function(array) {
+      let shuffledArray = array.slice();
+      for (let i = shuffledArray.length - 1; i > 0; i--) {
+          const j = Math.floor(Math.random() * (i + 1));
+          [shuffledArray[i], shuffledArray[j]] = [shuffledArray[j], shuffledArray[i]];
+      }
+      return shuffledArray;
+  });
+
   return {
     dir: {
       input: "src",
